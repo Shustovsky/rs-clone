e3e148faa89513be7e6076e43a99f02cf1a0c5cb
Alexander Shustovsky <99250085+Shustovsky@users.noreply.github.com>
2023-02-06 11:58:36 +0400

    Rc 7 add community  (#8)
    * feat: add loginNow banner
    
    * fix: fix bugs in slider
    
    * feat: add community slider

--- src/pages/mainPage/mainPageView.ts
+++ src/pages/mainPage/mainPageView.ts
@@ -1,16 +1,19 @@
 import { HeaderView } from '../../components/header/headerView';
 import { Slider } from './slider/slider';
 import { LoginNow } from './loginNow/loginNow';
+import { Community } from './community/community';
 
 export class MainPageView {
     private readonly header: HeaderView;
     private readonly slider: Slider;
     private readonly loginNow: LoginNow;
+    private readonly community: Community;
 
     constructor() {
         this.header = new HeaderView('#root');
         this.slider = new Slider('#root main');
         this.loginNow = new LoginNow('#root main');
+        this.community = new Community('#root main');
     }
 
     public render(): void {
@@ -22,5 +25,6 @@ export class MainPageView {
 
         this.slider.render();
         this.loginNow.render();
+        this.community.render();
     }
 }
