78a709c586e9fb4fd8cd6f7cb3edd1a78c6db2f1
NadzeyaShu <chunta89@gmail.com>
2023-02-06 13:42:25 +0400

    fix: refactoring

--- src/components/workoutCard/workoutCard.ts
+++ src/components/workoutCard/workoutCard.ts
@@ -2,15 +2,24 @@ import { Workout } from '../../model/Workout';
 import './workoutCard.scss';
 
 export class WorkoutCard {
-    public static create(workout: Workout): string {
+    private readonly selector: string;
+
+    constructor(selector: string) {
+        this.selector = selector;
+    }
+
+    public render(workout: Workout): void {
+        const card = <HTMLElement>document.createElement('div');
         const duration = Math.round(workout.duration / 60);
-        return ` <div class='workouts__card uk-height-medium uk-flex uk-flex-center uk-flex-middle uk-background-cover uk-light' data-src='${workout.coverImageUrl}' uk-img='loading: eager'>
-                    <div class='workouts__wrapper-title'>
-                         <div class='workouts__title'>${workout.title}</div>
-                         <div class='workouts__title subtitle'>${workout.subtitle}</div>
-                         <div class='workouts__info'>${duration} min - ${workout.difficulty} - ${workout.trainer.name}</div>
-                    </div>
-                    <div class='workouts__dimm'></div>
-            </div>`;
+        card.innerHTML = `<div class='workouts__card uk-height-medium uk-flex uk-flex-center uk-flex-middle uk-background-cover uk-light' data-src='${workout.coverImageUrl}' uk-img='loading: eager'>
+                            <div class='workouts__wrapper-title'>
+                                 <div class='workouts__title'>${workout.title}</div>
+                                 <div class='workouts__title subtitle'>${workout.subtitle}</div>
+                                 <div class='workouts__info'>${duration} min - ${workout.difficulty} - ${workout.trainer.name}</div>
+                            </div>
+                            <div class='workouts__dimm'></div>
+                         </div>`;
+
+        document.querySelector(this.selector)?.append(card);
     }
 }
--- src/pages/workouts/workoutsView.ts
+++ src/pages/workouts/workoutsView.ts
@@ -1,19 +1,23 @@
 import { Workout } from '../../model/Workout';
-import { Title } from '../../components/title/title';
-import { WorkoutCardList } from '../../components/worcoutcartList/workoutCardList';
+import { WorkoutsTitle } from '../../components/workoutsTitle/workoutsTitle';
+import { WorkoutCardList } from '../../components/workoutCartList/workoutCardList';
 
 export class WorkoutsView {
+    private readonly workoutCardList: WorkoutCardList;
+    private readonly workoutsTitle: WorkoutsTitle;
+
+    constructor() {
+        this.workoutCardList = new WorkoutCardList('#root main');
+        this.workoutsTitle = new WorkoutsTitle('#root main');
+    }
+
     public render(workouts: Workout[]): void {
         const root = <HTMLBodyElement>document.querySelector('#root')
         const main = document.createElement('main');
         main.className = 'workouts uk-container';
-
-        const title = Title.createWorkoutsTitle(workouts.length);
-        main.append(title);
-        const workoutList = WorkoutCardList.create(workouts);
-        title.after(workoutList);
-
-        document.body.append(main);
         root.append(main);
+
+        this.workoutsTitle.render(workouts.length);
+        this.workoutCardList.render(workouts);
     }
 }
