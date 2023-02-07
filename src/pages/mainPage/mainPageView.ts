ba04e8342920861b86c7e2ef2732698f3a5f3b11
Alexander Shustovsky <99250085+Shustovsky@users.noreply.github.com>
2023-02-07 11:23:34 +0400

    Rc 8 (#10)
    * fix: fix bug
    
    * feat: add banners section
    
    * refactor: add render

--- src/pages/mainPage/mainPageView.ts
+++ src/pages/mainPage/mainPageView.ts
@@ -2,18 +2,21 @@ import { HeaderView } from '../../components/header/headerView';
 import { Slider } from './slider/slider';
 import { LoginNow } from './loginNow/loginNow';
 import { Community } from './community/community';
+import { Banners } from './banners/banners';
 
 export class MainPageView {
     private readonly header: HeaderView;
     private readonly slider: Slider;
     private readonly loginNow: LoginNow;
     private readonly community: Community;
+    private readonly banners: Banners;
 
     constructor() {
         this.header = new HeaderView('#root');
         this.slider = new Slider('#root main');
         this.loginNow = new LoginNow('#root main');
         this.community = new Community('#root main');
+        this.banners = new Banners('#root main');
     }
 
     public render(): void {
@@ -26,5 +29,6 @@ export class MainPageView {
         this.slider.render();
         this.loginNow.render();
         this.community.render();
+        this.banners.render();
     }
 }
