5cad796de6ddb7f42cc968fb23ef4e0809b15cc6
DzmitrySm <magicjoker@yandex.ru>
2023-02-10 23:56:00 +0300

    feat: create account form

--- src/pages/profilePage/account/account.ts
+++ src/pages/profilePage/account/account.ts
@@ -2,6 +2,8 @@ import '../account/account.scss';
 import AccountTemplate from '../account/account.html';
 import Litepicker from 'litepicker';
 import autocomplete from 'autocompleter';
+import { IAccount } from '../../../mock/mockData';
+import { countries } from '../../../mock/mockData';
 
 export class AccountView {
     private readonly selector: string;
@@ -9,22 +11,56 @@ export class AccountView {
     constructor(selector: string) {
         this.selector = selector;
     }
-    public render() {
+    public render(account: IAccount) {
         const root = <HTMLBodyElement>document.querySelector(this.selector);
-        const account = <HTMLDivElement>document.createElement('div');
-        account.className = 'account_wrapper uk-container';
-        account.innerHTML = AccountTemplate;
-        root.append(account);
+        const profile = <HTMLDivElement>document.createElement('div');
+        profile.className = 'account_wrapper uk-container';
+        profile.innerHTML = `<h3 class="account">ABOUT YOU</h3>
+        <p class="account_text">The details you enter below will be used to calculate the calories you burn as accurately as possible, and to find events near you.</p>
+        <div class="account-img_wrapper uk-flex uk-flex-middle">
+            <div class="account-img"></div>
+            <span class="profile_add">Add profile picture</span>
+            <input type="file" class="input_add_img">
+        </div>
+        <form class="profile_form">
+            <div class="form_row uk-flex form_row_margin">
+                <div class="form_row_half">
+                    <input class="uk-input" type="text" placeholder="First and last name*" value=${account.name}>
+                </div>
+                <div class="form_row_half">
+                <input class="uk-input" type="email" placeholder="E-Mail*" value=${account.mail}>
+                </div>
+            </div>
+            <div class="form_row uk-flex form_row_margin_below">
+                <div class="form_row_half">
+                    <input class="uk-input search_country" id="autoComplete" type="search" placeholder="Country*" value=${account.country}>
+                </div>
+                <div class="form_row_half"></div>
+            </div>
+            <div class="form_row uk-flex form_row_margin">
+                <div class="form_row_half">
+                    <select class="uk-select" aria-label="Select">
+                        <option disabled selected>Gender</option>
+                        <option>Male</option>
+                        <option>Female</option>
+                        <option>Non binary</option>
+                    </select>
+                </div>
+            <div class="form_row_half">
+                <input class="uk-input uk-date litepicker" type="date">
+                <span class="icon_calendar" uk-icon="icon: calendar"></span>
+            </div>
+            </div>`;
+        const profileAddition = <HTMLDivElement>document.createElement('div');
+        profileAddition.innerHTML = AccountTemplate;
+        profile.append(profileAddition);
+        root.append(profile);
         const picker = new Litepicker({
             element: document.querySelector('.litepicker') as HTMLInputElement,
         });
-        const countries = [
-            { label: 'United Kingdom', value: 'UK' },
-            { label: 'United States', value: 'US' },
-        ];
 
         const input = document.querySelector('.search_country');
-
+        picker.setDate(account.birthday);
         autocomplete({
             input: input as HTMLInputElement,
             fetch: function (text, update) {
