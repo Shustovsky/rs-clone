986cafaa03cec2ec78a61e32a50adf6349685346
DzmitrySm <magicjoker@yandex.ru>
2023-02-10 21:11:09 +0300

    feat: add datepicker, autocomplete

--- src/pages/profilePage/account/account.ts
+++ src/pages/profilePage/account/account.ts
@@ -1,5 +1,7 @@
 import '../account/account.scss';
 import AccountTemplate from '../account/account.html';
+import Litepicker from 'litepicker';
+import autocomplete from 'autocompleter';
 
 export class AccountView {
     private readonly selector: string;
@@ -13,5 +15,26 @@ export class AccountView {
         account.className = 'account_wrapper uk-container';
         account.innerHTML = AccountTemplate;
         root.append(account);
+        const picker = new Litepicker({
+            element: document.querySelector('.litepicker') as HTMLInputElement,
+        });
+        const countries = [
+            { label: 'United Kingdom', value: 'UK' },
+            { label: 'United States', value: 'US' },
+        ];
+
+        const input = document.querySelector('.search_country');
+
+        autocomplete({
+            input: input as HTMLInputElement,
+            fetch: function (text, update) {
+                text = text.toLowerCase();
+                const suggestions = countries.filter((n) => n.label.toLowerCase().startsWith(text));
+                update(suggestions);
+            },
+            onSelect: function (item) {
+                (input as HTMLInputElement).value = item.label as string;
+            },
+        });
     }
 }
