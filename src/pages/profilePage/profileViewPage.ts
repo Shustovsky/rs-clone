463b88df6c8fc81302e510750a53c84cf6abe298
DzmitrySm <magicjoker@yandex.ru>
2023-02-13 19:37:58 +0300

    refactor: remove not use instances

--- src/pages/profilePage/profileViewPage.ts
+++ src/pages/profilePage/profileViewPage.ts
@@ -1,25 +1,16 @@
 import { HeaderView } from '../../components/header/headerView';
-import { ActivitiesView } from './activities/activitiesView';
-import { AccountView } from './account/account';
 import { SidebarView } from './sidebar/sidebarView';
 import { account } from '../../mock/mockData';
-import { AccountDeleteView } from './accountDelete/accountDeletView';
 import { SwitcherView } from './switcher/switcherView';
 
 export class ProfilePageView {
     private readonly header: HeaderView;
-    private readonly activities: ActivitiesView;
     private readonly sidebar: SidebarView;
-    private readonly account: AccountView;
-    private readonly accountDelete: AccountDeleteView;
     private readonly switcher: SwitcherView;
 
     constructor() {
         this.header = new HeaderView('#root');
         this.sidebar = new SidebarView('#root main');
-        this.activities = new ActivitiesView('#root main');
-        this.account = new AccountView('#root main');
-        this.accountDelete = new AccountDeleteView('#root main');
         this.switcher = new SwitcherView('#root main');
     }
 
