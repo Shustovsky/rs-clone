557b72c91309b1b6f47ce767820acd87d98d5a1d
NadzeyaShu <chunta89@gmail.com>
2023-02-16 16:32:26 +0400

    feat: add user profile

---
+++ src/model/Profile.ts
@@ -0,0 +1,52 @@
+export class Profile {
+    id: string;
+    email: string;
+    firstName: string | null = null;
+    lastName: string | null = null;
+    location: string | null = null;
+    gender: string | null = null;
+    birthday: Date | null = null;
+    weight: number | null = null;
+    height: number | null = null;
+    isPrivateOnlyMe = false;
+    isUnitsMetric = false;
+    isOffersNotificationEnabled = false;
+    isEmailNotificationEnabled = false;
+    workouts: ProfileWorkout[] = [];
+
+    constructor(id: string, email: string) {
+        this.id = id;
+        this.email = email;
+    }
+}
+
+export class ProfileWorkout {
+    id: string;
+    date: Date;
+    title: string;
+    imageUrl: string;
+    description: string;
+    calories: number;
+    duration: number;
+    count: number;
+
+    constructor(
+        id: string,
+        date: Date,
+        title: string,
+        imageUrl: string,
+        description: string,
+        calories: number,
+        duration: number,
+        count: number
+    ) {
+        this.id = id;
+        this.date = date;
+        this.title = title;
+        this.imageUrl = imageUrl;
+        this.description = description;
+        this.calories = calories;
+        this.duration = duration;
+        this.count = count;
+    }
+}
--- src/utils/language.ts
+++ src/utils/language.ts
@@ -1,18 +1,17 @@
 import { changeLanguage } from 'i18next';
 
-const DEFAULT_LANG = 'en'
+const DEFAULT_LANG = 'en';
 
 export enum SearchParams {
     lang = 'lang',
 }
 
-
 export const getLanguageFromLocation = (): string => {
     const searchParams = new URLSearchParams(window.location.search);
     return searchParams.get(SearchParams.lang) || DEFAULT_LANG;
-}
+};
 
 export const updateLanguage = async (): Promise<void> => {
     const currentLanguage = await getLanguageFromLocation();
     changeLanguage(currentLanguage);
-}
+};
