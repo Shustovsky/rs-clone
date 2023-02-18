210c5a3cc05e257fa01f63cb3fe49452e45798e6
A. Shustovsky <a.shustovsky@gmail.com>
2023-02-18 12:05:49 +0400

    feat: add loader to workouts and main

--- src/pages/workoutPage/workoutController.ts
+++ src/pages/workoutPage/workoutController.ts
@@ -1,25 +1,28 @@
 import { WorkoutService } from '../../service/workoutService';
 import { WorkoutView } from './workoutPageView';
 import { Workout } from '../../model/Workout';
+import { Loader } from '../../components/loader/Loader';
 
 export class WorkoutController {
-    workoutService: WorkoutService;
-    workoutView: WorkoutView;
+    private readonly workoutService: WorkoutService;
+    private readonly workoutView: WorkoutView;
+    private readonly loader: Loader;
 
     constructor(workoutService: WorkoutService, workoutView: WorkoutView) {
         this.workoutService = workoutService;
         this.workoutView = workoutView;
+        this.loader = new Loader();
     }
 
-    public async render(id: string) {
-        this.workoutView.createLoader();
+    public async render(id: string): Promise<void> {
+        this.loader.createLoader();
         try {
             const workout: Workout = await this.workoutService.fetchWorkout(id);
             this.workoutView.render(workout);
         } catch (e) {
             this.workoutView.createErrorBlock();
         } finally {
-            this.workoutView.deleteLoader();
+            this.loader.deleteLoader();
         }
     }
 }
--- src/pages/workoutPage/workoutPageView.ts
+++ src/pages/workoutPage/workoutPageView.ts
@@ -40,19 +40,6 @@ export class WorkoutView {
         this.workoutBtnStart.render(workout.id);
     }
 
-    public createLoader(): void {
-        const root = <HTMLBodyElement>document.querySelector('#root');
-
-        const loader = <HTMLElement>document.createElement('div');
-        loader.className = 'mask-loader uk-flex uk-flex-center';
-        loader.innerHTML = `<div uk-spinner='ratio: 4'></div>`;
-        root.append(loader);
-    }
-
-    public deleteLoader(): void {
-        document.querySelector(' #root .mask-loader')?.remove();
-    }
-
     public createErrorBlock(): void {
         const root = <HTMLBodyElement>document.querySelector('#root');
 
